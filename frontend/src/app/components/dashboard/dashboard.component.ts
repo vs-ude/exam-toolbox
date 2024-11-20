2fa77fab177fc408cc34eae455b6fffdb7f3f963
Max Brockmann <max@tbrockmann.de>
2024-11-20 17:49:20 +0100

    add Property binding for examCardComponent

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -3,22 +3,31 @@ import { MatButtonModule } from '@angular/material/button';
 import { DbService } from '../../services/db.service';
 import { MatIconModule } from '@angular/material/icon';
 import { ExamCardComponent } from '../exam-card/exam-card.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, ExamCardComponent],
+  imports: [MatButtonModule, MatIconModule, ExamCardComponent, NgFor],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
   constructor(
     private dbService: DbService
-  ){}
+  ) { }
+
+  public examNames = [
+    "Cloud, Web & Mobile",
+    "Rechnernetze",
+    "Betriebssysteme",
+    "Verteilte Systeme",
+    "Sicherheit in Kommunikationsnetzen",
+    "Rechnerarchitektur",
+  ]
 
 
-  onDBTest(){
+  onDBTest() {
     const name = 'Sample Exam'
     const comment = 'This is a sample exam comment'
     this.dbService.addExam(name, comment).subscribe(
--- frontend/src/app/components/exam-card/exam-card.component.ts
+++ frontend/src/app/components/exam-card/exam-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
@@ -9,5 +9,9 @@ import { MatIcon } from '@angular/material/icon';
   styleUrl: './exam-card.component.scss'
 })
 export class ExamCardComponent {
+  @Input() name!: string ; 
+
+  public lastEdited = 2; 
+
 
 }
