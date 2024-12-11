74e71ada07734b3030f2f738ff52809357a67113
Max Brockmann <max@tbrockmann.de>
2024-12-11 12:31:32 +0100

    add inputField for exam name

--- frontend/src/app/components/create-exam/create-exam.component.ts
+++ frontend/src/app/components/create-exam/create-exam.component.ts
@@ -1,8 +1,8 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, viewChild, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { TaskComponent } from './task/task.component';
-import { NgFor, NgStyle } from '@angular/common';
+import { NgFor, NgIf, NgStyle } from '@angular/common';
 
 
 
@@ -10,7 +10,7 @@ import { NgFor, NgStyle } from '@angular/common';
 @Component({
   selector: 'app-create-exam',
   standalone: true,
-  imports: [MatIconModule, MatTooltipModule, TaskComponent, NgFor],
+  imports: [MatIconModule, MatTooltipModule, TaskComponent, NgFor, NgIf, ],
   templateUrl: './create-exam.component.html',
   styleUrl: './create-exam.component.scss'
 })
@@ -19,7 +19,42 @@ export class CreateExamComponent {
   public inDropzone = false;
   private bodyElement: HTMLElement = document.body;
 
+  public examName = "New Exam"
   tasks:string[] = [];
+  public isNameChange = false
+
+  @ViewChild("nameInput") nameInput?:ElementRef;
+
+  @HostListener("document:click", ["$event"])
+  unselectInputs(event: MouseEvent) {
+    const elementId = (event.target as Element).id
+
+    if (elementId === "examName") {
+      return;
+    }
+    this.isNameChange = false;
+    if (!this.nameInput){return;}
+    const newName = this.nameInput.nativeElement.value;
+    if(newName === ""){return}
+    this.examName = newName
+  }
+
+
+
+  changeName(event:Event){
+    console.log(event)
+
+   
+  }
+
+  onNameChange(event:any){
+    if (event.key !== "Enter"){return}
+    const newName = event.target.value;
+    if(newName !== ""){
+      this.examName = event.target.value;
+    };
+    this.isNameChange = false;
+  }
 
   dragStart(event: DragEvent) {
     this.bodyElement.classList.add("inheritCursors");
@@ -37,6 +72,7 @@ export class CreateExamComponent {
   dragOverDropzone(event: DragEvent) {
     event.preventDefault();
   }
+
 }
 
 
