ef9a06c00328716712349e88b43ee43df8ecea77
Yasin Kavuk <yasin.kavuk@stud.uni-due.de>
2025-01-27 14:30:41 +0100

    const moved to global scope

--- frontend/src/app/components/debug/debug.component.ts
+++ frontend/src/app/components/debug/debug.component.ts
@@ -3,6 +3,55 @@ import { Exam, Task } from '../../exam';
 import { saveAs } from 'file-saver';
 import { ApiService } from '../../services/api.service';
 
+
+const examId = 'exam123';
+  const title = 'Cloud Web Mobile Development Exam';
+  const courseName = 'Introduction to Cloud, Web, and Mobile Development';
+  const examinerName = 'Dr. Jane Smith';
+  const semester = 'Fall 2025';
+  const date = '2025-12-15';
+  const examLengthMinutes = 120;
+    
+  const tasks:Task[] = [
+    {
+      taskId: 'task1',
+      type: 'shortAnswer',
+      question: {
+        DE: 'Was ist Cloud?',
+        EN: 'What is Cloud?'
+      },
+      solution: {
+        DE: '',
+        EN: ''
+      },
+      points: 4
+    },
+    {
+      taskId: 'task2',
+      type: 'shortAnswer',
+      question: {
+        DE: 'Was ist Web Mobile?',
+        EN: 'What is Web Mobile?'
+      },
+      solution: {
+        DE: '',
+        EN: ''
+      },
+      points: 6
+    }
+  ];
+
+  const exam: Exam = new Exam(
+    examId,
+    title,
+    courseName,
+    examinerName,
+    semester,
+    date,
+    examLengthMinutes,
+    tasks
+  );
+
 @Component({
   selector: 'app-debug',
   standalone: true,
@@ -17,54 +66,6 @@ export class DebugComponent {
   ) { }
 
   onAddExam() {
-    const examId = 'exam123';
-    const title = 'Cloud Web Mobile Development Exam';
-    const courseName = 'Introduction to Cloud, Web, and Mobile Development';
-    const examinerName = 'Dr. Jane Smith';
-    const semester = 'Fall 2025';
-    const date = '2025-12-15';
-    const examLengthMinutes = 120;
-    
-    const tasks:Task[] = [
-      {
-        taskId: 'task1',
-        type: 'shortAnswer',
-        question: {
-          DE: 'Was ist Cloud?',
-          EN: 'What is Cloud?'
-        },
-        solution: {
-          DE: '',
-          EN: ''
-        },
-        points: 4
-      },
-      {
-        taskId: 'task2',
-        type: 'shortAnswer',
-        question: {
-          DE: 'Was ist Web Mobile?',
-          EN: 'What is Web Mobile?'
-        },
-        solution: {
-          DE: '',
-          EN: ''
-        },
-        points: 6
-      }
-    ];
-
-    const exam: Exam = new Exam(
-      examId,
-      title,
-      courseName,
-      examinerName,
-      semester,
-      date,
-      examLengthMinutes,
-      tasks
-    );
-
     this.api.addExam(exam).subscribe(
       response => {
         console.log('Exam added successfully: ', response);
