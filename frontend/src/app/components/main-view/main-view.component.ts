8f92110b8f7c2b9fcf5ee6f39d34e73a049e2eb2
Max Brockmann <max@tbrockmann.de>
2025-09-02 14:39:44 +0200

    Add navigation collapse functionality and improve styling in main view

--- frontend/src/app/components/main-view/main-view.component.ts
+++ frontend/src/app/components/main-view/main-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatListModule } from '@angular/material/list'
 import { RouterOutlet, RouterModule } from '@angular/router';
@@ -6,24 +6,30 @@ import { MatButtonModule } from '@angular/material/button'
 import { ThemeToggleService } from '../../services/theme-toggle.service';
 import { MatIcon } from '@angular/material/icon';
 import { ApiService } from '../../services/api.service';
+import { MatTooltip } from "@angular/material/tooltip";
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-main-view',
   standalone: true,
-  imports: [MatSidenavModule, MatListModule, RouterOutlet, MatButtonModule, RouterModule, MatIcon],
+  imports: [MatSidenavModule, MatListModule, RouterOutlet, MatButtonModule, RouterModule, MatIcon, MatTooltip, NgIf],
   templateUrl: './main-view.component.html',
   styleUrl: './main-view.component.scss'
 })
 export class MainViewComponent {
-  username: string = ""
-  initialLetter: string = ""
+  public username: string = ""
+  public initialLetter: string = ""
+  public isNavCollapsed: boolean = false
+  @ViewChild('sidenav') sidenav!: ElementRef;
+  @ViewChild('content') content!: ElementRef;
 
   constructor(
     private themeToggleService: ThemeToggleService,
     private api: ApiService
-  ){}
+  ) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.api.getUser().subscribe(
       user => {
         this.username = user.id
@@ -32,7 +38,7 @@ export class MainViewComponent {
     )
   }
 
-  onLogout(){
+  onLogout() {
     console.log("-- Logging out --")
     window.location.href = '/auth/logout'
   }
@@ -41,8 +47,19 @@ export class MainViewComponent {
     this.themeToggleService.toggleTheme();
   }
 
-  setUsername(name: string){
+  setUsername(name: string) {
     this.username = name
     this.initialLetter = name[0]
   }
+
+  toggleNavCollapse() {
+    this.isNavCollapsed = !this.isNavCollapsed
+    this.sidenav.nativeElement.style.width = this.isNavCollapsed ? '0' : '12%';
+    this.content.nativeElement.style.width = this.isNavCollapsed ? '100%' : '88%';
+
+    console.log(this.sidenav.nativeElement.style.width)
+
+  }
+
+
 }
