194e08eb10f35e526584b5380e303a2a82ab80bc
Max Brockmann <max@tbrockmann.de>
2025-09-18 10:50:43 +0200

    Update router configuration to use hash location strategy; #79

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { ApiService } from './services/api.service';
 
 import { routes } from './app.routes';
@@ -9,7 +9,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
+    provideRouter(routes, withHashLocation()), 
     provideHttpClient(),
     ApiService, provideAnimationsAsync(), 
   ], 
