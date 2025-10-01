957e63b0a9d2b4f63b13d0beb9d6534c7149ceef
Max Brockmann <max@tbrockmann.de>
2025-10-01 11:17:16 +0200

    Add dummy sorting, filtering, and view options to Downloadable Exams UI

--- frontend/src/app/components/downloadable-exams/downloadable-exams.component.ts
+++ frontend/src/app/components/downloadable-exams/downloadable-exams.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ExamCardComponent } from '../exam-card/exam-card.component';
 import { Exam } from '../../exam';
 import { ApiService, DownloadableJob } from '../../services/api.service';
 import { saveAs } from 'file-saver';
 import { LoadingService } from '../../services/loading.service';
+import { MatIcon } from '@angular/material/icon';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-downloadable-exams',
   standalone: true,
   imports: [
     ExamCardComponent,
-
+    MatIcon,
+    NgClass,
   ],
   templateUrl: './downloadable-exams.component.html',
   styleUrl: './downloadable-exams.component.scss'
@@ -19,6 +22,20 @@ export class DownloadableExamsComponent {
 
   public exams: Exam[] = [new Exam("Test Course", "Test Examiner", "WS 2023/24", "2024-10-10", 90, [], "testId")];
   private downloadableJobs: DownloadableJob[] = [];
+
+  public showDropdowns = { sorting: false, filter: false, }
+  @HostListener("document:click", ["$event"])
+  handleDropdownStates(event: MouseEvent) {
+    const elementId = (event.target as Element).id
+
+    if (elementId === "openSortingDropdown") {
+      this.showDropdowns.sorting = !this.showDropdowns.sorting;
+    } else { this.showDropdowns.sorting = false; }
+
+    if (elementId === "openFilterDropdown") {
+      this.showDropdowns.filter = !this.showDropdowns.filter;
+    } else { this.showDropdowns.filter = false; }
+  }
 
   constructor(private api: ApiService, private loader: LoadingService) {
     this.api.getDownloadableJobs().subscribe(downloadableJobs => {
@@ -32,7 +49,7 @@ export class DownloadableExamsComponent {
     })
   }
 
-  onDownload(examId: string|undefined) {
+  onDownload(examId: string | undefined) {
     if (!examId) {
       console.error("Exam ID is undefined");
       return;
@@ -49,7 +66,7 @@ export class DownloadableExamsComponent {
 
   }
 
-  private downloadExam(jobId: string){
+  private downloadExam(jobId: string) {
     this.loader.loadingOn();
     this.api.downloadMassExamResult(jobId).subscribe({
       next: (zipBlob: Blob) => {
