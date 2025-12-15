9a43fc7bbed64ea7b7cfd11f93f47d25a2e090df
YasinKavuk <yasinkavuk100@gmail.com>
2025-12-15 01:41:26 +0100

    dashboard now shows the 9 most recently modified exams by that user

--- backend/exam.ts
+++ backend/exam.ts
@@ -1,122 +1,121 @@
 import { Tag } from "./tag.ts";
 
 export class Exam {
-    _id?: string = undefined;
-    courseName: string;
-    examinerName: string;
-    semester: string;
-    date: string;
-    examLengthMinutes: number;
-    tasks: TaskGroup[];
+  _id?: string = undefined;
+  courseName: string;
+  examinerName: string;
+  semester: string;
+  date: string;
+  examLengthMinutes: number;
+  tasks: TaskGroup[];
+  public lastEditedBy?: string;
+  public updatedAt?: Date;
 
-    constructor(
-        courseName: string,
-        examinerName: string,
-        semester: string,
-        date: string,
-        examLengthMinutes: number,
-        tasks: TaskGroup[],
-        _id?: string,
-    ) {
-        this.courseName = courseName;
-        this.examinerName = examinerName;
-        this.semester = semester;
-        this.date = date;
-        this.examLengthMinutes = examLengthMinutes;
-        this.tasks = tasks;
-    }
+  constructor(
+    courseName: string,
+    examinerName: string,
+    semester: string,
+    date: string,
+    examLengthMinutes: number,
+    tasks: TaskGroup[],
+    _id?: string,
+  ) {
+    this.courseName = courseName;
+    this.examinerName = examinerName;
+    this.semester = semester;
+    this.date = date;
+    this.examLengthMinutes = examLengthMinutes;
+    this.tasks = tasks;
+  }
 }
 
 export interface TaskGroup {
-    groupNumber: number;
-    groupTitle: Translation;
-    tasks: Task[];
+  groupNumber: number;
+  groupTitle: Translation;
+  tasks: Task[];
 }
 
-
 export type Task =
-    | MultipleChoiceTask
-    | ShortAnswerTask
-    | PictureTask
-    | LatexTask
-    | TableTask
-    | ManualText
-    | NewPage
-    ;
-
+  | MultipleChoiceTask
+  | ShortAnswerTask
+  | PictureTask
+  | LatexTask
+  | TableTask
+  | ManualText
+  | NewPage;
 
 export interface BaseTask {
-    taskId: string;
-    type: string;
-    question: Question;
-    points: number;
-    tags: Tag[];
-    createdBy: string;
-    createdAt: Date;
-    lastUsed: Date;
-    usedIn: string[];
-    parent?: string;
-    children: string[];
+  taskId: string;
+  type: string;
+  question: Question;
+  points: number;
+  tags: Tag[];
+  createdBy: string;
+  createdAt: Date;
+  lastUsed: Date;
+  usedIn: string[];
+  parent?: string;
+  children: string[];
 }
 
 export interface Question {
-    DE: string;
-    EN: string;
+  DE: string;
+  EN: string;
 }
 
 export interface MultipleChoiceTask extends BaseTask {
-    type: "multipleChoice";
-    answerOptions: AnswerOptions[];
+  type: "multipleChoice";
+  answerOptions: AnswerOptions[];
 }
 
 export interface AnswerOptions {
-    DE: string;
-    EN: string;
-    correct: boolean;
+  DE: string;
+  EN: string;
+  correct: boolean;
 }
 
 export interface ShortAnswerTask extends BaseTask {
-    type: "shortAnswer";
-    solution: Translation;
+  type: "shortAnswer";
+  solution: Translation;
 }
 
 export interface PictureTask extends BaseTask {
-    type: "pictureTask";
-    questionPicture: Image;
-    solutionPicture: Image;
+  type: "pictureTask";
+  questionPicture: Image;
+  solutionPicture: Image;
 }
 
 export interface Image {
-    urlDE: string;
-    urlEN: string;
-    altTextDE?: string;
-    altTextEN?: string;
+  urlDE: string;
+  urlEN: string;
+  altTextDE?: string;
+  altTextEN?: string;
 }
 
 export interface LatexTask extends BaseTask {
-    type: "latex";
-    questionLatex: Translation;
+  type: "latex";
+  questionLatex: Translation;
 }
 
 export interface TableTask extends BaseTask {
-    type: "table"
-    tableHeadersQuestion: Translation[];
-    tableDataQuestion: Translation[][];
-    tableHeadersSolution: Translation[];
-    tableDataSolution: Translation[][];
+  type: "table";
+  tableHeadersQuestion: Translation[];
+  tableDataQuestion: Translation[][];
+  tableHeadersSolution: Translation[];
+  tableDataSolution: Translation[][];
 }
 
 export interface ManualText extends BaseTask {
-    type: "manualText"
-    points: 0; // Manual texts do not have points
+  type: "manualText";
+  points: 0; // Manual texts do not have points
 }
 
 export interface NewPage extends BaseTask {
-    type: "newPage"
-    points: 0; // New pages do not have points
+  type: "newPage";
+  points: 0; // New pages do not have points
 }
 
 export interface Translation {
-    DE: string;
-    EN: string;
+  DE: string;
+  EN: string;
 }
--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,77 +1,73 @@
-import { Component, HostListener } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, HostListener } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 import { ExamCardComponent } from "../exam-card/exam-card.component";
-import { NgClass, NgFor } from '@angular/common';
-import { Router } from '@angular/router';
-import { ApiService } from '../../services/api.service';
-import { Exam } from '../../exam';
-
+import { NgClass, NgFor } from "@angular/common";
+import { Router } from "@angular/router";
+import { ApiService } from "../../services/api.service";
+import { Exam } from "../../exam";
 
 @Component({
-  selector: 'app-dashboard',
+  selector: "app-dashboard",
   standalone: true,
   imports: [MatButtonModule, MatIconModule, ExamCardComponent, NgFor, NgClass],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  templateUrl: "./dashboard.component.html",
+  styleUrl: "./dashboard.component.scss",
 })
 export class DashboardComponent {
-
-  exams: Exam[] = []
-
+  recentExams: Exam[] = [];
 
   constructor(
     private router: Router,
-    private api: ApiService
-  ) { }
+    private api: ApiService,
+  ) {}
 
-
-  ngOnInit(){
-    this.api.getExams().subscribe(res => {
-      this.exams = res
-      console.table(this.exams)
-    })
+  ngOnInit() {
+    this.api.getRecentExams().subscribe((exams) => {
+      this.recentExams = exams;
+    });
   }
-
 
   @HostListener("document:click", ["$event"])
   handleDropdownStates(event: MouseEvent) {
-    const elementId = (event.target as Element).id
+    const elementId = (event.target as Element).id;
 
     if (elementId === "openSortingDropdown") {
       this.showDropdowns.sorting = !this.showDropdowns.sorting;
-    } else { this.showDropdowns.sorting = false; }
+    } else {
+      this.showDropdowns.sorting = false;
+    }
 
     if (elementId === "openFilterDropdown") {
       this.showDropdowns.filter = !this.showDropdowns.filter;
-    } else { this.showDropdowns.filter = false; }
+    } else {
+      this.showDropdowns.filter = false;
+    }
   }
 
-  public showDropdowns = { sorting: false, filter: false, }
+  public showDropdowns = { sorting: false, filter: false };
 
-  onAddExam(){
-    this.router.navigate(['/create-exam']);  
+  onAddExam() {
+    this.router.navigate(["/create-exam"]);
   }
 
-  onTaskPool(){
-    this.router.navigate(['/task-pool']);
+  onTaskPool() {
+    this.router.navigate(["/task-pool"]);
   }
 
-  onDebug(){
-    this.router.navigate(['/debug'])
+  onDebug() {
+    this.router.navigate(["/debug"]);
   }
 
-  onExamsPool(){
-    this.router.navigate(['/exams-pool']);
+  onExamsPool() {
+    this.router.navigate(["/exams-pool"]);
   }
 
-  onExamClick(exam: Exam){
-    if(exam._id == undefined){
+  onExamClick(exam: Exam) {
+    if (exam._id == undefined) {
       console.warn(`Exam ${exam.courseName} ${exam.semester} has no ExamID`);
       return;
-    };
-    this.router.navigate([`/create-exam/${exam._id}`])
-    
+    }
+    this.router.navigate([`/create-exam/${exam._id}`]);
   }
-
 }
