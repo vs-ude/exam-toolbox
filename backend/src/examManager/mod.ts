a4a3084b24b0d8c67b617a4b02e950db814e4a17
Oskar Carl <oskar.carl@uni-due.de>
2026-04-29 18:07:17 +0200

    fix(backend): properly export tag type Was borked during a rebase

--- backend/src/examManager/mod.ts
+++ backend/src/examManager/mod.ts
@@ -2,7 +2,7 @@ export { configureExamManagerRouter } from "./router.ts";
 export { configureTaskPoolRouter } from "./taskPoolRouter.ts";
 export { configureTagRouter } from "./tagRouter.ts";
 export type { Exam, Task, Task as TaskType, TaskGroup } from "./exam.ts";
-export { Tag } from "./tag.ts";
+export type { Tag } from "./tag.ts";
 export {
   generateExam,
   generateTasksLatex,
--- backend/src/examManager/tag.ts
+++ backend/src/examManager/tag.ts
@@ -1,7 +1,6 @@
-
 export interface Tag {
-    _id?: string;
-    name: string;
-    color: string;
-    textColor: string;
+  _id?: string;
+  name: string;
+  color: string;
+  textColor: string;
 }
