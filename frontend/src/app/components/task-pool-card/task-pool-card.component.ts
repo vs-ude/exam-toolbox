ba4c47e2489cb0d1fbbed5e6e4acf11964f4fe1b
Oskar Carl <oskar.carl@uni-due.de>
2026-07-08 13:12:18 +0200

    frontend: fix task pool card icon

--- frontend/src/app/components/task-pool-card/task-pool-card.component.ts
+++ frontend/src/app/components/task-pool-card/task-pool-card.component.ts
@@ -1,29 +1,32 @@
-import { Component, Input, input } from '@angular/core';
-import { Task } from '../../exam';
-import { MatIconModule } from '@angular/material/icon';
-import { ColorProviderService } from '../../services/color-provider.service';
-import { NgFor, NgStyle } from '@angular/common';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Component, Input, input } from "@angular/core";
+import { Task } from "../../exam";
+import { MatIconModule } from "@angular/material/icon";
+import { ColorProviderService } from "../../services/color-provider.service";
+import { NgFor, NgStyle } from "@angular/common";
+import { MatDialog, MatDialogRef } from "@angular/material/dialog";
 import {
   AddTagDialogComponent,
   AddTagDialogData,
-} from '../add-tag-dialog/add-tag-dialog.component';
-import { Tag } from '../../tag';
-import { ApiService } from '../../services/api.service';
-import { TagHelperService } from '../../services/tag-helper.service';
+} from "../add-tag-dialog/add-tag-dialog.component";
+import { Tag } from "../../tag";
+import { ApiService } from "../../services/api.service";
+import { TagHelperService } from "../../services/tag-helper.service";
+import { environment } from "../../../environments/environment";
 
 @Component({
-  selector: 'app-task-pool-card',
+  selector: "app-task-pool-card",
   standalone: true,
   imports: [MatIconModule, NgStyle],
-  templateUrl: './task-pool-card.component.html',
-  styleUrl: './task-pool-card.component.scss',
+  templateUrl: "./task-pool-card.component.html",
+  styleUrl: "./task-pool-card.component.scss",
 })
 export class TaskPoolCardComponent {
-  @Input() task!: Task;
-  public taskColor: string = '';
+  @Input()
+  task!: Task;
+  public taskColor: string = "";
   public mouseHoveringCard: boolean = false;
   public mouseHoveringTagIndex = -1;
+  public publicPath = environment.publicPath;
 
   constructor(
     private colorProvider: ColorProviderService,
@@ -46,12 +49,12 @@ export class TaskPoolCardComponent {
   public onAddTag() {
     const dialogRef: MatDialogRef<AddTagDialogComponent, AddTagDialogData> =
       this.dialog.open(AddTagDialogComponent, {
-        width: '50%',
-        height: '50%',
+        width: "50%",
+        height: "50%",
         data: {},
       });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (!result) {
         return;
       }
